6f551beb16950f9d99246c1131eeba0eb8be02f7
isabelysgsg <silvaisabely788@gmail.com>
2024-09-03 10:17:35 -0300

    PLATAFORMA

--- Meu Game 3D/Assets/Scrip´ts/Player.cs
+++ Meu Game 3D/Assets/Scrip´ts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +9,32 @@ using MonoBehaviour = UnityEngine.MonoBehaviour;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forçaPulo = 7;
+    private bool noChao;
+    
     public Rigidbody rb;
-    public int forcaPulo = 10;
-    public bool noChao = true;
-
+    
     void Start()
     {
         TryGetComponent(out rb);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionExit(Collision other)
     {
-        if (collision.gameObject.tag == "chão")
+        if (!noChao && other.gameObject.tag == "Chão")
         {
             noChao = true;
+        }
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+            
         }
     }
 
@@ -32,8 +45,21 @@ public class Player : MonoBehaviour
         
         Vector3 direcao = new Vector3(h,0,v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
-        if (transform.position.y < -5)
+
+        if (Input.GetKeyDown(KeyCode.Space) &&  noChao)
         {
+            rb.AddForce(Vector3.up * forçaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+        
+        
+        
+        
+        if (transform.position.y < -5)
+            
+            
+        {
+            
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
